5102b74bb2d71637891c7521f1e9e954052c145d
hanshuobo <1432101420@qq.com>
2024-02-17 09:58:46 +0800

    设置倒计时定时器

---
+++ js/game.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    var startSign = $(".start-sign")
+    var countdownBox = $(".countdown-box")
+    var countdown = $(".countdown")
+
+    setTimeout(function () {
+        startSign.style.display = "none"
+        var countdownBoxWidth = countdownBox.offsetWidth
+
+        var intervalId = setInterval(function () {
+            // console.log(1);
+
+            // 递减宽度，这里可以根据需要调整递减的速度
+            var newWidth = countdownBoxWidth - 1;
+
+            // 设置新宽度
+            countdown.style.width = newWidth + "px";
+
+            // 如果宽度为零，清除定时器
+            if (newWidth <= 0) {
+                clearInterval(intervalId);
+            }
+        }, 1000); // 时间间隔为1秒，可以根据需要调整
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }, 1500);
+
+}
