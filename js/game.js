8971fc91fc2819800408f911d70ffdf34abb905f
hanshuobo <1432101420@qq.com>
2024-02-18 20:23:10 +0800

    第一次交互

--- js/game.js
+++ js/game.js
@@ -7,20 +7,20 @@ window.onload = () => {
         startSign.style.display = "none"
         var countdownBoxWidth = countdownBox.offsetWidth
 
-        var intervalId = setInterval(function () {
-            // console.log(1);
+        // var intervalId = setInterval(function () {
+        //     // console.log(1);
 
-            // 递减宽度，这里可以根据需要调整递减的速度
-            var newWidth = countdownBoxWidth - 1;
+        //     // 递减宽度，这里可以根据需要调整递减的速度
+        //     var newWidth = countdownBoxWidth - 1;
 
-            // 设置新宽度
-            countdown.style.width = newWidth + "px";
+        //     // 设置新宽度
+        //     countdown.style.width = newWidth + "px";
 
-            // 如果宽度为零，清除定时器
-            if (newWidth <= 0) {
-                clearInterval(intervalId);
-            }
-        }, 1000); // 时间间隔为1秒，可以根据需要调整
+        //     // 如果宽度为零，清除定时器
+        //     if (newWidth <= 0) {
+        //         clearInterval(intervalId);
+        //     }
+        // }, 1000); // 时间间隔为1秒，可以根据需要调整
 
 
 
